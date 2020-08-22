70d138af18ee5ea40a4c85e6922c1b4ce0ff59a4
grimcake <994459124@qq.com>
2020-08-22 23:18:23 +0800

    create project

---
+++ coolblog/src/main/java/com/grimcake/coolblog/CoolblogApplication.java
@@ -0,0 +1,13 @@
+package com.grimcake.coolblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolblogApplication.class, args);
+    }
+
+}
---
+++ coolblog/src/test/java/com/grimcake/coolblog/CoolblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grimcake.coolblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
